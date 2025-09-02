18478c13dc18589732f29c361e5cd3be23bbcc49
Usman Sadiq <usadiq@cs.dal.ca>
2025-09-02 17:06:22 -0300

    Corrected some variable names

--- CREDIT RISK PROJECT/app.py
+++ CREDIT RISK PROJECT/app.py
@@ -33,7 +33,7 @@ input_df = pd.DataFrame({
     "Job" : [job],
     "Housing": [encoders["Housing"].transform([housing])[0]],
     "Saving accounts": [encoders["Saving accounts"].transform([saving_accounts])[0]],
-    "Checking account": [encoders["checking account"].transform([checking_account])[0]],
+    "Checking account": [encoders["Checking account"].transform([checking_account])[0]],
     "Credit amount" : [credit_amount],
     "Duration" : [duration]
 }
